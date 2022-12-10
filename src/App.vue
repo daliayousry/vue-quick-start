d1d7e454a03dee686cd476960db2b3996537fc3a
Dalia Ihab <dalia.ihab@gmail.com>
2022-12-10 18:54:04 +0200

    Single File Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <the-header></the-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader
   }
 }
 </script>
