233a8bd2e2321378c0b2eb2fabe8d8544e23c2de
Dalia Ihab <dalia.ihab@gmail.com>
2022-12-10 18:54:31 +0200

    Templates

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,18 @@
 <template>
 <div>
     <p> This is my header</p>
+    <p>{{hello=='hi'?'nice':'okay'}}</p>
 </div>
 </template>
 
 <script>
 export default {
     name: 'TheHeader',
+    data(){
+        return{
+            hello:'hi'
+        }
+    }
     
 }
 </script>
