ad3d1a6083a9f0287c97b1abb4da6b2abbea510f
Dalia Ihab <dalia.ihab@gmail.com>
2022-12-17 14:06:21 +0200

    v-for

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,35 +4,14 @@
         <h2 class="vue-todo-list my-4">✨ Vue Todo List</h2>
     </div>
     <b-card class="todo-list-card">
+        <input v-model="name" @change="setName()" placeholder="add my name"/>
+
         <b-list-group>
-            <b-list-group-item> Laundary
+            <b-list-group-item class="list-group" v-for="todo in todos" :key="todo.id">
+                {{todo.task}}
                 <div class="todo-list-action">
-                    <i class="fa-regular fa-heart todo-action-icon"></i>
-                    <i class="fa-regular fa-trash-can todo-action-icon"></i>
-                </div>
-            </b-list-group-item>
-            <b-list-group-item>Read new Book
-                <div class="todo-list-action">
-                    <i class="fa-regular fa-heart todo-action-icon"></i>
-                    <i class="fa-regular fa-trash-can todo-action-icon"></i>
-                </div>
-            </b-list-group-item>
-            <b-list-group-item>Clean the house
-                <div class="todo-list-action">
-                    <i class="fa-regular fa-heart todo-action-icon"></i>
-                    <i class="fa-regular fa-trash-can todo-action-icon"></i>
-                </div>
-            </b-list-group-item>
-            <b-list-group-item>Complete the previous Task
-                <div class="todo-list-action">
-                    <i class="fa-regular fa-heart todo-action-icon"></i>
-                    <i class="fa-regular fa-trash-can todo-action-icon"></i>
-                </div>
-            </b-list-group-item>
-            <b-list-group-item>Update my CV
-                <div class="todo-list-action">
-                    <i class="fa-regular fa-heart todo-action-icon"></i>
-                    <i class="fa-regular fa-trash-can todo-action-icon"></i>
+                    <i class="fa-regular fa-heart todo-action-icon fav-icon" v-on:click="setFavTask(todo.id)"></i>
+                    <i class="fa-regular fa-trash-can todo-action-icon delete-icon"></i>
                 </div>
             </b-list-group-item>
         </b-list-group>
@@ -51,7 +30,32 @@ export default {
     name: 'TheHeader',
     data() {
         return {
-
+            todos: [{
+                    task: 'Laundry',
+                    id: '1',
+                    isFav: false
+                },
+                {
+                    task: 'Read new Book',
+                    id: '2',
+                    isFav: false
+                },
+                {
+                    task: 'Complete the previous Task',
+                    id: '3',
+                    isFav: false
+                },
+            ],
+            name:''
+        }
+    },
+    methods:{
+        setFavTask(id){
+            this.todos[id].isFav= true;
+            console.log("todo",this.todos[id])
+        },
+        setName(){
+            console.log("change in my input")
         }
     }
 
@@ -90,9 +94,17 @@ export default {
 .todo-list-action {
     display: inline-block;
     float: right;
+    cursor: pointer;
 }
-.todo-action-icon{
-        margin-right: 10px;
+
+.todo-action-icon {
+    margin-right: 10px;
     font-size: 20px;
+}
+.fav-icon:hover{
+  color: hotpink;
+}
+.delete-icon:hover{
+   color: blue;
 }
 </style>
